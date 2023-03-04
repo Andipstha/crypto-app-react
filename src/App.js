ea75fd2b69fc88fa7217b30f2f3c22506b6e9ee4
andipstha <sandip.stha120@gmail.com>
2023-03-04 20:06:28 +0545

    added routes

--- src/App.js
+++ src/App.js
@@ -2,8 +2,8 @@ import React, { useState, useEffect } from 'react'
 import axios from 'axios'
 import { Routes, Route } from 'react-router-dom'
 import Coins from './components/Coins.js'
-import Coin from './routes/Coin'
-import Navbar from './components/Navbar'
+import Coin from './routes/Coin.js'
+import Navbar from './components/Navbar.js'
 
 
 function App() {
